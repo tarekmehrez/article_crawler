f6ccddaedd0eda8528899a5215a0de9ab37dc5c3
Tarek Mehrez <tarekmmehrez@gmail.com>
2015-12-26 13:49:11 +0200

    updated run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,20 @@
 # Author: Tarek
 
 now=$(date +%Y-%m-%d:%H:%M:%S)
-for spider in $(scrapy list); do
-	scrapy crawl $spider 
+cd /var/crawlers/article_crawler
+echo "Starting cron activity at $now, please wait..." > /var/crawlers/article_crawler/l$
+
+cuttoff=$1
+for spider in $(scrapy list | head -$1 | tail -6); do
+        scrapy crawl $spider
 
 done
-python clusterRelated.py
-python updateIndex.py
+
+#python clusterRelated.py
+# python updateIndex.py
+
+echo "finished cron activity at $now" > /var/crawlers/article_crawler/logs
+
+
+
+
